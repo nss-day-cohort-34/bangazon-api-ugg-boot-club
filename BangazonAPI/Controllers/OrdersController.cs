c8a0ec485f69bafd3025cda9786cf574562071b0
Matthew Ross <matthew.douglas.ross@gmail.com>
2019-11-06 16:28:46 -0600

    going back in timegit status!

---
+++ BangazonAPI/Controllers/OrdersController.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public OrdersController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: api/Orders
+        
+        // 1. When an order is deleted, every line item(i.e.entry in OrderProduct) should be removed
+        // 2. Should be able to filter out completed orders with the ?completed=false query string parameter.If the parameter value is true, then only completed order should be returned.
+        // 3. If the query string parameter of? _include = products is in the URL, then the list of products in the order should be returned.
+        // 4. If the query string parameter of? _include = customers is in the URL, then the customer representation should be included in the response.
+
+      [HttpGet]
+        public async Task<IActionResult> Get(string completed)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT o.Id AS OrderId, o.CustomerId, o.PaymentTypeId, o.Status FROM [Order] o ";
+                    if (completed?.ToLower() == "true")
+                    {
+                        cmd.CommandText += "WHERE Status LIKE '%omplete%'";
+                    }
+                    else if (completed?.ToLower() == "false")
+                    {
+                        cmd.CommandText += "WHERE Status NOT LIKE '%omplete%'";
+                    }
+
+                    
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    List<Order> orders = new List<Order>();
+                    while (reader.Read())
+                    {
+                        Order order = new Order()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("OrderId")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            PaymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId")),
+                            Status = reader.GetString(reader.GetOrdinal("Status")),
+                        };
+
+                        orders.Add(order);
+                    }
+
+
+                    reader.Close();
+
+                    return Ok(orders);
+                }
+            }
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}", Name = "GetOrder")]
+        public async Task<IActionResult> Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, CustomerId, PaymentTypeId, Status
+                                        FROM [Order]
+                                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    Order order = null;
+                    if (reader.Read())
+                    {
+                        order = new Order
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            PaymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId")),
+                            Status = reader.GetString(reader.GetOrdinal("Status")),
+                        };
+                    }
+
+                    reader.Close();
+
+                    return Ok(order);
+                }
+            }
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Order order)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO [Order] (CustomerId, PaymentTypeId, Status)
+                        OUTPUT INSERTED.Id
+                        VALUES (@customerId, @paymentTypeId, @status)
+                    ";
+                    cmd.Parameters.Add(new SqlParameter("@customerId", order.CustomerId));
+                    cmd.Parameters.Add(new SqlParameter("@paymentTypeId", order.PaymentTypeId));
+                    cmd.Parameters.Add(new SqlParameter("@status", order.Status));
+
+
+                    order.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtRoute("GetOrder", new { id = order.Id }, order);
+                }
+            }
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Order order)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                            UPDATE [Order]
+                            SET CustomerId = @customerId, PaymentTypeId = @paymentTypeId, Status = @status
+                            WHERE Id = @id
+                        ";
+                        cmd.Parameters.Add(new SqlParameter("@customerId", order.CustomerId));
+                        cmd.Parameters.Add(new SqlParameter("@paymentTypeId", order.PaymentTypeId));
+                        cmd.Parameters.Add(new SqlParameter("@status", order.Status));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!OrderExists(id) || !OrderProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        //add the following line to command text when it's time to implement DELETE on associated OrderProducts
+                        cmd.CommandText = @"
+                            DELETE FROM [Order] WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+
+                // Call helper method here to delete from OrderProducts table
+                //DeleteAssociatedOrderProduct(id);
+            }
+            catch (Exception)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> DeleteAssociatedOrderProduct(int id)
+        //{
+        //    try
+        //    {
+        //        using (SqlConnection conn = Connection)
+        //        {
+        //            conn.Open();
+        //            using (SqlCommand cmd = conn.CreateCommand())
+        //            {
+        //                cmd.CommandText = @"DELETE FROM OrderProduct op WHERE op.OrderId = @id";
+        //                cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                int rowsAffected = cmd.ExecuteNonQuery();
+        //                if (rowsAffected > 0)
+        //                {
+        //                    return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                }
+        //                throw new Exception("No rows affected");
+        //            }
+        //        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        if (!OrderProductExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //}
+
+        private bool OrderExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id FROM [Order] WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    return reader.Read();
+                }
+            }
+        }
+        private bool OrderProductExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id FROM OrderProduct WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
---
+++ SQLQuery3.sql
@@ -0,0 +1,15 @@
+﻿--SELECT c.Id, c.FirstName, c.LastName, c.CreationDate, c.LastActiveDate, p.Id AS ProductId, p.AcctNumber, p.Name
+--FROM Customer c INNER JOIN PaymentType p ON p.CustomerId = c.Id
+
+/*SELECT o.Id, o.CustomerId, o.Status, c.FirstName, c.LastName, pt.Name, pt.AcctNumber	
+FROM [Order] o
+	LEFT JOIN Customer c ON c.Id = o.CustomerId
+	LEFT JOIN PaymentType pt ON pt.Id = o.PaymentTypeId;*/
+
+	--SELECT o.Id AS OrderId, o.CustomerId, o.PaymentTypeId, o.Status FROM [Order] o
+
+	DELETE 
+	--[o.Id, o.CustomerId, o.PaymentTypeId, o.Status, op.Id AS Order_Product_Id, op.OrderId, op.ProductId]
+	FROM [Order] o INNER JOIN OrderProduct op ON op.OrderId = o.Id WHERE o.Id = 1 ; 
+
+	Select o.Id, o.CustomerId, o.PaymentTypeId, o.Status, op.Id AS Order_Product_Id, op.OrderId, op.ProductId from [Order] o INNER JOIN OrderProduct op ON op.OrderId = o.Id;
