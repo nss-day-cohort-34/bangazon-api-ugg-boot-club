309e0ec97779e0a7b0215bbf934ac2238af2c8ce
Haroon Iqbal <haroonsiqbal@gmail.com>
2019-11-06 15:24:58 -0600

    added employee controller

---
+++ BangazonAPI/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+using BangazonAPI.Models;
+
+namespace BangazonAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+        public bool IsSuperVisor { get; set; }
+        [Required]
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Department Department { get; set; }
+        public Computer Computer { get; set; }
+}
+}
