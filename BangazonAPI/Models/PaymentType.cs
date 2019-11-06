f88d66dd21222ca2435709a58546222e3f281a5b
unknown <ellieash4000@gmail.com>
2019-11-06 13:58:04 -0600

    get/put/post test working

---
+++ BangazonAPI/Models/PaymentType.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace BangazonAPI.Models
+{
+    public class PaymentType
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string AcctNumber { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int CustomerId { get; set; }
+
+    }
+}
---
+++ TestBangazonAPI/TestPaymentType.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+using Xunit;
+using BangazonAPI.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace TestBangazonAPI
+{
+    public class TestPaymentType
+    {
+        [Fact]
+        public async Task Test_Modify_PaymentType()
+        {
+            // New last name to change to and test
+            string newAcctNumber = "000111";
+
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    PUT section
+                */
+                PaymentType modifiedJoe = new PaymentType
+                {
+                    Name = "Amex",
+                    AcctNumber = newAcctNumber,
+                    CustomerId = 1,
+                };
+                var modifiedJoeAsJSON = JsonConvert.SerializeObject(modifiedJoe);
+
+                var response = await client.PutAsync(
+                    "/api/paymenttype/1",
+                    new StringContent(modifiedJoeAsJSON, Encoding.UTF8, "application/json")
+                );
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
+
+
+                /*
+                    GET section
+                    Verify that the PUT operation was successful
+                */
+                var getJoe = await client.GetAsync("/api/paymenttype/1");
+                getJoe.EnsureSuccessStatusCode();
+
+                string getJoeBody = await getJoe.Content.ReadAsStringAsync();
+                PaymentType newJoe = JsonConvert.DeserializeObject<PaymentType>(getJoeBody);
+
+                Assert.Equal(HttpStatusCode.OK, getJoe.StatusCode);
+                Assert.Equal(newAcctNumber, newJoe.AcctNumber);
+            }
+        }
+        [Fact]
+        public async Task Test_Get_All_PaymentTypes()
+        {
+            using (var client = new APIClientProvider().Client)
+            {
+                /*
+                    ARRANGE
+                */
+
+
+                /*
+                    ACT
+                */
+                var response = await client.GetAsync("/api/paymenttype");
+
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var paymentTypes = JsonConvert.DeserializeObject<List<PaymentType>>(responseBody);
+                /*
+                    ASSERT
+                */
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                Assert.True(paymentTypes.Count > 0);
+            }
+
+        }
+    }
+}
