3b11ac9ee82012342b86333300433c0183ea25e8
Haroon Iqbal <haroonsiqbal@gmail.com>
2019-11-07 14:13:14 -0600

    worked on department

---
+++ BangazonAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public DepartmentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        private SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        // GET api/Department
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name, Budget FROM Department";
+
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    Dictionary<int, Department> departments = new Dictionary<int, Department>();
+                    while (reader.Read())
+                    {
+                        int departmentId = reader.GetInt32(reader.GetOrdinal("Id"));
+                        if (!departments.ContainsKey(departmentId))
+                        {
+
+
+                            Department department = new Department
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+
+                            };
+
+                            departments.Add(departmentId, department);
+                        }
+                        Department fromDictionary = departments[departmentId];
+                    }
+
+                    reader.Close();
+
+                    return Ok(departments.Values);
+                }
+            }
+        }
+
+        // GET api/employee/5
+        [HttpGet("{id}", Name = "GetDepartment")]
+        public async Task<IActionResult> Get(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Name, Budget FROM Department
+                                        WHERE e.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    Department department = null;
+                    if (reader.Read())
+                    {
+                        department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+
+                        };
+                    }
+                    reader.Close();
+
+                    return Ok(department);
+                }
+            }
+        }
+
+        // POST api/employee
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // More string interpolation
+                    cmd.CommandText = @"
+                        INSERT INTO Employee (FirstName, LastName, DepartmentId, IsSuperVisor, StartDate, EndDate)
+                        OUTPUT INSERTED.Id
+                        VALUES (@firstName, @lastName, @departmentId, @isSuperVisor, @startDate, @endDate)
+                    ";
+                    cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@lastName", employee.LastName));
+                    cmd.Parameters.Add(new SqlParameter("@departmentId", employee.DepartmentId));
+                    cmd.Parameters.Add(new SqlParameter("@isSuperVisor", employee.IsSuperVisor));
+                    cmd.Parameters.Add(new SqlParameter("@startDate", employee.StartDate));
+                    cmd.Parameters.Add(new SqlParameter("@endDate", employee.EndDate));
+
+                    employee.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    return CreatedAtRoute("GetEmployee", new { id = employee.Id }, employee);
+                }
+            }
+        }
+
+        // PUT api/employee/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"
+                            UPDATE Employee
+                            SET FirstName = @firstName, LastName = @lastName, DepartmentId = @departmentId, IsSuperVisor = @isSuperVisor, StartDate = @startDate, EndDate = @endDate
+                            WHERE Id = @id
+                        ";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+                        cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                        cmd.Parameters.Add(new SqlParameter("@lastName", employee.LastName));
+                        cmd.Parameters.Add(new SqlParameter("@departmentId", employee.DepartmentId));
+                        cmd.Parameters.Add(new SqlParameter("@isSuperVisor", employee.IsSuperVisor));
+                        cmd.Parameters.Add(new SqlParameter("@startDate", employee.StartDate));
+                        cmd.Parameters.Add(new SqlParameter("@endDate", employee.EndDate));
+
+                        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        // DELETE api/employee/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Employee WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id FROM Employee WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    return reader.Read();
+                }
+            }
+        }
+    }
+}
--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,10 +1,1 @@
-SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, e.IsSuperVisor, e.StartDate, e.EndDate,
-			d.Name,
-			c. Make, C.Manufacturer
-FROM EMPLOYEE e
-INNER JOIN Department d on d.Id = e.DepartmentId
-LEFT JOIN Computer c on c.CurrentEmployeeId = e.Id;
-
-
-SELECT * FROM DEPARTMENT
-SELECT * FROM COMPUTER
+SELECT * FROM PaymentType
